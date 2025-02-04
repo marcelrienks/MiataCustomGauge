a2e1215d614d4330e7f9ba34389c00dcec036e10
Marcel Rienks <marcelrienks@gmail.com>
2025-02-04 12:25:20 +0200

    Cleaned up constants

--- src/main.cpp
+++ src/main.cpp
@@ -36,18 +36,18 @@ public:
       cfg.pin_rst = RST;
       cfg.pin_busy = -1;
 
-      cfg.memory_width = WIDTH;
-      cfg.memory_height = HEIGHT;
-      cfg.panel_width = WIDTH;
-      cfg.panel_height = HEIGHT;
-      cfg.offset_x = OFFSET_X;
-      cfg.offset_y = OFFSET_Y;
+      cfg.memory_width = SCREEN_WIDTH;
+      cfg.memory_height = SCREEN_HEIGHT;
+      cfg.panel_width = SCREEN_WIDTH;
+      cfg.panel_height = SCREEN_HEIGHT;
+      cfg.offset_x = SCREEN_OFFSET_X;
+      cfg.offset_y = SCREEN_OFFSET_Y;
       cfg.offset_rotation = 0;
       cfg.dummy_read_pixel = 8;
       cfg.dummy_read_bits = 1;
       cfg.readable = false;
       cfg.invert = true;
-      cfg.rgb_order = RGB_ORDER;
+      cfg.rgb_order = SCREEN_RGB_ORDER;
       cfg.dlen_16bit = false;
       cfg.bus_shared = false;
 
@@ -70,27 +70,22 @@ public:
   }
 };
 
-LGFX tft;
-Preferences prefs;
+LGFX device;
+Preferences preferences;
 
-// Screens
-lv_obj_t *home_screen;
-
-//components
-Meter *meter;
-
-//screen stuff
-static const uint32_t screenWidth = WIDTH;
-static const uint32_t screenHeight = HEIGHT;
-
-//constants
+//TODO: pull this out into a static class
+// Constants
 lv_color_t black = lv_color_make(0, 0, 0);
 
-const unsigned int lvBufferSize = screenWidth * 10;
-uint8_t lvBuffer[2][lvBufferSize];
+//TODO: convert this to a class object, with lv_obj_t and custom screen functions below
+// Screens
+lv_obj_t *screen;
 
-// Screen
-const uint8_t DEFAULT_BRIGHTNESS = 100;
+// Components
+Meter *meter;
+
+const unsigned int lvBufferSize = SCREEN_WIDTH * 10;
+uint8_t lvBuffer[2][lvBufferSize];
 
 void set_needle_line_value(void * obj, int32_t v)
 {
@@ -99,7 +94,7 @@ void set_needle_line_value(void * obj, int32_t v)
 
 void screenBrightness(uint8_t value)
 {
-  tft.setBrightness(value);
+  device.setBrightness(value);
 }
 
 // TODO: Can this be put in an external class?
@@ -109,21 +104,21 @@ void my_disp_flush(lv_display_t *display, const lv_area_t *area, unsigned char *
   uint32_t h = lv_area_get_height(area);
   lv_draw_sw_rgb565_swap(data, w * h);
 
-  if (tft.getStartCount() == 0)
+  if (device.getStartCount() == 0)
   {
-    tft.endWrite();
+    device.endWrite();
   }
 
-  tft.pushImageDMA(area->x1, area->y1, area->x2 - area->x1 + 1, area->y2 - area->y1 + 1, (uint16_t *)data);
+  device.pushImageDMA(area->x1, area->y1, area->x2 - area->x1 + 1, area->y2 - area->y1 + 1, (uint16_t *)data);
   lv_disp_flush_ready(display);
 }
 
 // Make Home Screen
 void make_home_screen()
 {
-  home_screen = lv_obj_create(NULL);
-  lv_obj_set_style_bg_color(home_screen, black, LV_PART_MAIN);
-  lv_obj_set_style_bg_opa(home_screen, LV_OPA_COVER, LV_PART_MAIN);
+  screen = lv_obj_create(NULL);
+  lv_obj_set_style_bg_color(screen, black, LV_PART_MAIN);
+  lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, LV_PART_MAIN);
 }
 
 void setup()
@@ -132,34 +127,33 @@ void setup()
   {
     Serial.begin(115200);
 
-    int rt = prefs.getInt("rotate", 0);
-    int br = prefs.getInt("brightness", 100);
+    int rt = preferences.getInt("rotate", 0);
 
     // initialise screen
-    tft.init();
-    tft.initDMA();
-    tft.startWrite();
-    tft.fillScreen(TFT_BLACK);
-    tft.setRotation(rt);
+    device.init();
+    device.initDMA();
+    device.startWrite();
+    device.fillScreen(TFT_BLACK);
+    device.setRotation(rt);
 
     lv_init(); // initialise LVGL
 
     // setup screen
-    static auto *lvDisplay = lv_display_create(screenWidth, screenHeight);
+    static auto *lvDisplay = lv_display_create(SCREEN_WIDTH, SCREEN_HEIGHT);
     lv_display_set_color_format(lvDisplay, LV_COLOR_FORMAT_RGB565);
     lv_display_set_flush_cb(lvDisplay, my_disp_flush);
     lv_display_set_buffers(lvDisplay, lvBuffer[0], lvBuffer[1], lvBufferSize, LV_DISPLAY_RENDER_MODE_PARTIAL);
 
-    screenBrightness(br); // startup brightness
+    screenBrightness(SCREEN_DEFAULT_BRIGHTNESS);
 
     make_home_screen();
     
     //init Meter
-    meter = new Meter(home_screen);
+    meter = new Meter(screen);
     meter->register_animation_cb(set_needle_line_value);
     meter->build();
 
-    lv_scr_load(home_screen);
+    lv_scr_load(screen);
 
     Serial.print("Setup finished");
   }
